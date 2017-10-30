bed882a0d084a928937942385d6614c2fcd52656
jameskun77 <wangchao104999@126.com>
2017-10-30 19:21:43 +0800

    -add Merge Sorted List

---
+++ src/mergeSortedList/merge.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+public:
+	int value;
+	struct ListNode* next;
+};
+
+class Solution
+{
+public:
+	ListNode* Merge(ListNode* pLeft,ListNode* pRight)
+	{
+		if(pLeft == NULL)
+		{
+			cout<<"Left is NULL"<<endl;
+			return pRight;
+		}
+		else if(pRight == NULL)
+		{
+			cout<<"Right is NULL"<<endl;
+		}
+		
+		ListNode* left = pLeft;
+		ListNode* right = pRight;
+		
+		ListNode* head = NULL;
+		if(left->value < right->value)
+		{
+			head = left;
+			left = left->next;
+			cout <<head->value <<"in list" <<endl;
+		}
+		else
+		{
+			head = right;
+			right = right->next;
+			cout<<head->value<<"in list" <<endl;
+		}
+		
+		ListNode *curr = head;
+		  
+		while(left != NULL && right != NULL)
+        {
+            cout <<"left = " <<left->value <<", right = " <<right->value <<endl;
+
+            if(left->value < right->value)
+            {
+                cout <<left->value <<"in list" <<endl;
+
+                curr->next = left;
+                curr = curr->next;
+
+                left = left->next;
+            }
+            else
+            {
+                cout <<right->value <<"in list" <<endl;
+
+                curr->next = right;
+                curr = curr->next;
+
+                right = right->next;
+            }
+        }
+
+        if(left != NULL)
+        {
+            curr->next = left;
+        }
+        else
+        {
+            curr->next = right;
+        }
+
+
+        return head;
+	}
+};
+
+int main()
+{
+    ListNode left, right[3];
+
+    left.value = 5;
+    left.next = NULL;
+
+    right[0].value = 1;
+    right[0].next = &right[1];
+    right[1].value = 2;
+    right[1].next = &right[2];
+    right[2].value = 4;
+    right[2].next = NULL;
+
+
+    Solution solu;
+    ListNode *head = solu.Merge(&left, right);
+    while(head != NULL)
+    {
+        cout <<head->value;
+        head = head->next;
+    }
+
+
+    return 0;
+}
