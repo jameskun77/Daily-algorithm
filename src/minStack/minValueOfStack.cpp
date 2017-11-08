fd5297c6de5e474836e3c131a5e91eb2d0b6f829
jameskun77 <wangchao104999@126.com>
2017-11-08 19:49:39 +0800

    - min value of stack

---
+++ src/minStack/minValueOfStack.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class Solution
+{
+public:
+	void push(int val)
+	{
+		this->dataStack.push(val);
+
+		if (this->minStack.size() == 0 || val < this->minStack.top())
+		{
+			this->minStack.push(val);
+		}
+		else
+		{
+			this->minStack.push(this->minStack.top());
+		}
+	}
+
+	void pop()
+	{
+		if (this->dataStack.size() > 0 && this->dataStack.size() > 0)
+		{
+			this->dataStack.pop();
+			this->minStack.pop();
+		}
+	}
+
+	int top()
+	{
+		if (this->dataStack.size() > 0)
+		{
+			return this->dataStack.top();
+		}
+		else
+		{
+			cout << "data stack size is zero" << endl;
+			return -1;
+		}
+	}
+
+	int min()
+	{
+		if (this->minStack.size() > 0)
+		{
+			return this->minStack.top();
+		}
+		else
+		{
+			cout << "min stack size is zero" << endl;
+			return -1;
+		}
+	}
+
+
+protected:
+	stack<int> minStack;
+	stack<int> dataStack;
+};
+
+int main()
+{
+	Solution solu;
+	solu.push(100);
+	solu.push(457);
+	solu.push(777);
+	solu.push(233);
+
+
+	cout <<"stack min value: "<< solu.min() << endl;
+	cout << "stack top value: " << solu.top() << endl;
+}
