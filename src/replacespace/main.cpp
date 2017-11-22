ab989e172dbb72aaabc6f32e15db4f8e74024256
jameskun77 <wangchao104999@126.com>
2017-11-22 17:19:00 +0800

    replace space %20

---
+++ src/replacespace/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+void replaceSpace(char* pChar, int len)
+{
+	int count = 0;
+	for (int i = 0; i < len; ++i)
+	{
+		if (pChar[i] == ' ')
+			++count;
+	}
+
+	int newLen = len + count * 2;
+
+	pChar[newLen] = '\0';
+
+	int behand = len - 1;
+	int end = newLen - 1;
+	for (; behand >= 0 && end >= 0;)
+	{
+		if (pChar[behand] == ' ')
+		{
+			pChar[end--] = '0';
+			pChar[end--] = '2';
+			pChar[end--] = '%';
+			behand--;
+		}
+		else
+		{
+			pChar[end--] = pChar[behand--];
+		}
+	}
+
+}
+
+int main()
+{
+	char arr[20] = "a b c d";
+	replaceSpace(arr, 7);
+	cout << arr << endl;
+}
