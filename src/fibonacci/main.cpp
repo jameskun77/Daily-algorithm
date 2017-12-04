5e12777d9216c0d49965fbd9ee469619597389a3
Jameskun77 <wangchao104999@126.com>
2017-12-04 10:35:34 +0800

    + fibonacci

---
+++ src/fibonacci/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+long long Fibonacci(unsigned n)
+{
+	int result[2] = { 0, 1 };
+	if (n < 2)
+	{
+		return result[n];
+	}
+
+	long long fibNMinusOne = 1;
+	long long fibNMinusTwo = 0;
+	long long fibN = 0;
+
+	for (int i = 2; i <= n; i++)
+	{
+		fibN = fibNMinusOne + fibNMinusTwo;
+
+		fibNMinusTwo = fibNMinusOne;
+		fibNMinusOne = fibN;
+	}
+
+	return fibN;
+}
+
+int main()
+{
+	cout <<"sizeof long "<< sizeof(long) << endl;
+	cout << "sizeof long long " << sizeof(long long) << endl;
+
+	cout << Fibonacci(10) << endl;
+}
