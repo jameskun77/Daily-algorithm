2d3b81cebcf36ea364a80cb08318820128ce2425
Jameskun77 <wangchao104999@126.com>
2017-12-06 14:48:39 +0800

    tree is balance find sum path

---
+++ src/findPath/pathOfSum.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct BinaryTree
+{
+	int value;
+	BinaryTree* mLeft;
+	BinaryTree* mRight;
+};
+
+void findPathCore(BinaryTree* pNode,int expectSum,vector<int>& path,int curSum)
+{
+	curSum += pNode->value;
+	path.push_back(pNode->value);
+
+	bool isLeaf = (pNode->mLeft == NULL) && (pNode->mLeft == NULL);
+	if (curSum == expectSum && isLeaf)
+	{
+		cout << "A path is found: " << endl;
+		auto itor = path.begin();
+		for (; itor != path.end(); ++itor)
+		{
+			cout << *itor << " ";
+		}
+
+		cout << endl;
+	}
+
+	if (pNode->mLeft)
+	{
+		findPathCore(pNode->mLeft, expectSum, path, curSum);
+	}
+
+	if (pNode->mRight)
+	{
+		findPathCore(pNode->mRight, expectSum, path, curSum);
+	}
+
+	curSum -= pNode->value;
+	path.pop_back();
+}
+
+void findPath(BinaryTree* pRoot, int excepedSum)
+{
+	if (NULL == pRoot)
+	{
+		return;
+	}
+
+	vector<int> path;
+	int currSum = 0;
+	findPathCore(pRoot, excepedSum, path, currSum);
+}
+
+
+int main()
+{
+	return 0;
+}
---
+++ src/treeDepth/treeBalance.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+using namespace std;
+
+struct BinaryTree
+{
+	int value;
+	BinaryTree* mLeft;
+	BinaryTree* mRight;
+};
+
+int TreeDepth(BinaryTree* pNode)
+{
+	if (NULL == pNode)
+	{
+		return 0;
+	}
+	
+	int leftLen = 0;
+	if (pNode->mLeft)
+		leftLen = TreeDepth(pNode->mLeft);
+
+	int rightLen = 0;
+	if (pNode->mRight)
+		rightLen = TreeDepth(pNode->mRight);
+
+	return (leftLen > rightLen) ? leftLen + 1 : rightLen + 1;
+
+}
+
+bool isBalance(BinaryTree* pNode)
+{
+	if (pNode == NULL)
+	{
+		return true;
+	}
+
+	int leftLen = 0;
+	if (pNode->mLeft)
+		leftLen = TreeDepth(pNode->mLeft);
+
+	int rightLen = 0;
+	if (pNode->mRight)
+		rightLen = TreeDepth(pNode->mRight);
+
+	int diff = leftLen - rightLen;
+	if (diff > 1 || diff < -1)
+	{
+		return false;
+	}
+
+	return isBalance(pNode->mLeft) && isBalance(pNode->mRight);
+}
+
+bool isBalanceCore(BinaryTree* pRoot,int* pDepth)
+{
+	if (NULL == pRoot)
+	{
+		*pDepth = 0;
+		return true;
+	}
+
+	int left, right;
+	if (isBalanceCore(pRoot->mLeft, &left) && isBalanceCore(pRoot->mRight, &right))
+	{
+		int diff = left - right;
+		if (diff <= 1 && diff >= -1)
+		{
+			*pDepth = 1 + (left > right ? left : right);
+			return true;
+		}
+	}
+	return false;
+}
+
+bool isBalance1(BinaryTree* pRoot)
+{
+	int depth;
+	return isBalanceCore(pRoot, &depth);
+}
