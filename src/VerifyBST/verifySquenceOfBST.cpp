72c41a0263a8b7dfffab574c26e9fb43799c7379
Jameskun77 <wangchao104999@126.com>
2017-12-06 11:40:45 +0800

    verify bst

---
+++ src/VerifyBST/verifySquenceOfBST.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+bool verifySquenceOfBST(int arr[], int len)
+{
+	if (NULL == arr || len <= 0)
+	{
+		cout << "input invalid" << endl;
+		return false;
+	}
+
+	int root = arr[len - 1];
+	int i = 0;
+	for (; i < len - 1;i++)
+	{
+		if (arr[i] > root)
+		{
+			break;
+		}
+	}
+
+	int j = i;
+	for (; j < len - 1;j++)
+	{
+		if (arr[j] < root)
+		{
+			return false;
+		}
+	}
+
+	bool left = true;
+	if (i > 0)
+	{
+		left = verifySquenceOfBST(arr, i);
+	}
+
+	bool right = true;
+	if (i < len - 1)
+	{
+		right = verifySquenceOfBST(arr, i);
+	}
+
+	return left && right;
+}
+
+int main()
+{
+	int arr1[] = { 5, 7, 6, 9, 11, 10, 8 };
+	int arr2[] = { 7, 4, 6, 5 };
+
+	cout << verifySquenceOfBST(arr2, 4)<<endl;
+	cout << verifySquenceOfBST(arr1, 7)<<endl;
+}
---
+++ src/binaryTreeMirror/MirrorTree.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct BinaryTree
+{
+	int value;
+	BinaryTree* mLeft;
+	BinaryTree* mRight;
+};
+
+void swap(int& a, int& b)
+{
+	a = a ^ b;
+	b = b ^ a;
+	a = a ^ b;
+}
+
+
+void MirrorRecursive(BinaryTree* pTree)
+{
+	if (NULL == pTree)
+	{
+		return;
+	}
+
+	if (NULL == pTree->mLeft && NULL == pTree->mRight)
+	{
+		return;
+	}
+
+	BinaryTree* tmp = pTree->mLeft;
+	pTree->mLeft = pTree->mRight;
+	pTree->mRight = pTree->mLeft;
+
+	if (pTree->mLeft)
+	{
+		MirrorRecursive(pTree->mLeft);
+	}
+
+	if (pTree->mRight)
+	{
+		MirrorRecursive(pTree->mRight);
+	}
+}
+
+void MirrorPre(BinaryTree* pTree)
+{
+	if (NULL == pTree)
+	{
+		return;
+	}
+
+	if (NULL == pTree->mLeft && NULL == pTree->mRight)
+	{
+		return;
+	}
+
+	queue<BinaryTree *> binaryTreeQueue;
+	BinaryTree* pNode = pTree;
+
+	while (pNode)
+	{
+		swap(pNode->mLeft, pNode->mRight);
+		if (pNode->mLeft)
+			binaryTreeQueue.push(pNode->mLeft);
+
+		if (pNode->mRight)
+			binaryTreeQueue.push(pNode->mRight);
+
+		if (!binaryTreeQueue.empty())
+		{
+			pNode = binaryTreeQueue.front();
+			binaryTreeQueue.pop();
+		}
+		else
+		{
+			break;
+		}
+	}
+}
