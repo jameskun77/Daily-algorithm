79cce092976d24402085a183d4527a752caa327d
Jameskun77 <wangchao104999@126.com>
2017-12-08 09:50:29 +0800

    -

---
+++ src/deletChar/deleteChar.cpp
@@ -0,0 +1,62 @@
+//从一个字符串中删除在第二个字符串中出现的字符
+//input："We are students.", "aeiou"
+//output: W r Stdnts.
+
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+	void deleteChar(char* pSorce, const char* pDelete)
+	{
+		if (NULL == pSorce || NULL == pDelete)
+		{
+			return;
+		}
+
+		const unsigned int tableSize = 256;
+		int hashTable[tableSize];
+		memset(hashTable, 0, sizeof(hashTable));
+
+		const char* pTemp = pDelete;
+		while ('\0' != *pTemp)
+		{
+			hashTable[*pTemp] = 1;
+			++pTemp;
+		}
+
+		char* pFast = pSorce;
+		char* pSlow = pSorce;
+
+		while ('\0' != *pFast)
+		{
+			if (1 != hashTable[*pFast])
+			{
+				*pSlow = *pFast;
+				++ pSlow;
+			}
+
+			++ pFast;
+		}
+
+		*pSlow = '\0';
+	}
+};
+
+int main()
+{
+	//char* pSrc = "They are students."; //全局内存变量 不允许修改  报错
+	//char* pDst = "aeiou";
+
+	char pSrc[] = "They are students.";
+	char pDst[] = "aeiou";
+
+	cout << pSrc << endl;
+	cout << "after delete:" << endl;
+
+	Solution solu;
+	solu.deleteChar(pSrc, pDst);
+	cout << pSrc << endl;
+}
