80cae5af267849b5163fa2527c09f24d4a7317b5
Jameskun77 <wangchao104999@126.com>
2017-12-13 19:57:22 +0800

    两个队列实现一个栈

--- src/firstNotRepeatChar/solution.cpp
+++ src/firstNotRepeatChar/solution.cpp
@@ -31,7 +31,6 @@ public:
 			}
 		}
 
-		return -1;
 	}
 };
 
--- src/useTwoStackImpDeque/TwoStackImpQueue.cpp
+++ src/useTwoStackImpDeque/TwoStackImpQueue.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stack>
+#include <queue>
 
 using namespace std;
 
@@ -41,6 +42,65 @@ protected:
 	stack<int> mOutStack;
 };
 
+class Stack
+{
+public:
+
+	void push(int x)
+	{
+		if (!q1.empty())
+		{
+			q2.push(x);
+			while (!q1.empty())
+			{
+				q2.push(q1.front());
+				q1.pop();
+			}
+		}
+		else
+		{
+			q1.push(x);
+			while (!q2.empty())
+			{
+				q1.push(q2.front());
+				q2.pop();
+			}
+		}
+	}
+
+	void pop()
+	{
+		if (q1.empty() && q2.empty())
+		{
+			throw new exception("stack is empty");
+		}
+		else if (!q1.empty())
+			q1.pop();
+		else
+			q2.pop();
+	}
+
+	int top()
+	{
+		if (!q1.empty())
+		{
+			return q1.front();
+		}
+		else
+		{
+			return q2.front();
+		}
+	}
+
+	bool empty()
+	{
+		return (q1.empty() && q2.empty());
+	}
+
+private:
+	queue<int> q1, q2;
+};
+
 int main()
 {
 	Solution solu;
@@ -49,4 +109,13 @@ int main()
 	solu.push(7);
 
 	cout << "first value is: " << solu.pop() << endl;
+
+	Stack stk;
+	stk.push(1);
+	stk.push(2);
+	stk.pop();
+	stk.push(3);
+	cout << stk.top() << endl;
+	stk.push(4);
+	cout << stk.top() << endl;
 }
